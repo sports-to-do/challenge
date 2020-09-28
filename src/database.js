21bf82b960c5c5623536e55f05a14a07752d0c85
Maureen Barahona <maureenbarahona@gmail.com>
2020-09-28 11:56:25 -0600

    config of database and express handlebars

--- src/database.js
+++ src/database.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://testing:Aa123456!@cluster0.slm1h.azure.mongodb.net/std-db-app?retryWrites=true&w=majority',{
     useCreateIndex:true,
     useNewUrlParser:true,
-    useFindAndModify:false
+    useFindAndModify:false,
+    useUnifiedTopology:true
 })
 .then(db => console.log('DB is connected'))
 .catch(err => console.error(err));
