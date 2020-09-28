1ab1584e18d8ddab2c029c75309bc696746d86b0
Maureen Barahona <maureenbarahona@gmail.com>
2020-09-28 16:54:06 -0600

    config of port process.env.PORT

--- src/server.js
+++ src/server.js
@@ -1,4 +1,23 @@
 const  express =  require('express');
+
+
+//Initializations
 const app = express();
 
+//Settings
+
+app.set('port', process.env.PORT || 4000);
+
+//Middlewares
+
+//Global Variables
+
+
+
+//Routes
+
+//Static files
+
+
 module.exports = app;
+
