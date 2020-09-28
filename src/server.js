3c88dc74e1d8791cc4fa5a8b534817236b2e35f7
Maureen Barahona <maureenbarahona@gmail.com>
2020-09-28 17:00:35 -0600

    Static files definition

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const  express =  require('express');
+const { restart } = require('nodemon');
+const path = require('path');
 
 
 //Initializations
@@ -7,17 +9,23 @@ const app = express();
 //Settings
 
 app.set('port', process.env.PORT || 4000);
+app.set('views',path.join(__dirname,'views'));
 
 //Middlewares
+
+app.use(express.urlencoded({extended:false}));
 
 //Global Variables
 
 
 
 //Routes
+app.get('/', (req, res)=>{
+ res.send('Hola');
+});
 
 //Static files
-
+app.use(express.static(path.join(__dirname,'public')));
 
 module.exports = app;
 
