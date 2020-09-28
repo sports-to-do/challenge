b9c3c8cb59958612d4b7d72754f623ca54dc3241
Maureen Barahona <maureenbarahona@gmail.com>
2020-09-28 11:12:34 -0600

    basic configuration for file type of hbs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 const express =require ('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const session = require('express-session');
+
 //Initialiazations
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT ||3000);
-
+app.set('views', path.join(__dirname,'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir:path.join(app.get('views'), 'partials'),
+    extname:'./hbs'
+}));
+app.set('view engine', '.hbs');
 //Middlewares
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+app.use(session({
+    secret:'mysecretapp',
+    resave: true,
+    saveUninitialized: true
+}));
+
 
 // Global Variables
 
