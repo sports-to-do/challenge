15d8e952d513368473836068051c7e2229198d81
Maureen Barahona <maureenbarahona@gmail.com>
2020-09-28 19:19:28 -0600

    def routes

--- src/server.js
+++ src/server.js
@@ -30,9 +30,7 @@ app.use(express.urlencoded({extended:false}));
 
 
 //Routes
-app.get('/', (req, res)=>{
- res.render('index');
-});
+app.use(require('./routes/index.routes'));
 
 //Static files
 app.use(express.static(path.join(__dirname,'public')));
