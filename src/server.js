24b5a158be3463388f776ab367cd2e1148896bda
Maureen Barahona <maureenbarahona@gmail.com>
2020-09-29 13:34:58 -0600

    create helpers for autentication

--- src/server.js
+++ src/server.js
@@ -5,11 +5,13 @@ const morgan = require('morgan');
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
+const passport = require('passport');
 
 
 
 //Initializations
 const app = express();
+require('./config/passport');
 
 //Settings
 
@@ -36,6 +38,8 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
 
 //Global Variables
@@ -43,6 +47,7 @@ app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 });
 
