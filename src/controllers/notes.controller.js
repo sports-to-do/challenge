c0e5bd171b2fbda8afadf71c0db8f254b135b5f1
Maureen Barahona <maureenbarahona@gmail.com>
2020-09-30 23:40:21 -0600

    review label

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -26,7 +26,7 @@ notesCtrl.createNewNote = async (req, res) => {
     const newNote = new Note({ title, description });
     newNote.user = req.user.id;
     await newNote.save();
-    req.flash('success_msg', 'Note Added Successfully');
+    req.flash('success_msg', 'Event Added Successfully');
     res.redirect('/notes');
   }
 };
@@ -50,13 +50,13 @@ notesCtrl.renderEditForm = async (req, res) => {
 notesCtrl.updateNote = async (req, res) => {
   const { title, description } = req.body;
   await Note.findByIdAndUpdate(req.params.id, { title, description });
-  req.flash('success_msg', 'Note Updated Successfully');
+  req.flash('success_msg', 'Event Updated Successfully');
   res.redirect('/notes');
 };
 
 notesCtrl.deleteNote = async (req, res) => {
   await Note.findByIdAndDelete(req.params.id);
-  req.flash('success_msg', 'Note Deleted Successfully');
+  req.flash('success_msg', 'Event Deleted Successfully');
   res.redirect('/notes');
 };
 
